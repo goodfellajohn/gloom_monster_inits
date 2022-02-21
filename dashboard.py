20b5f5ff91162e4fd93e3bc023f11783c5ec4e04
goodfellajohn <48392434+goodfellajohn@users.noreply.github.com>
2022-02-21 18:17:44 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,8 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 from PIL import Image
-from tabluate import tabulate
+#from tabluate 
+import tabulate
 
 
 st.set_page_config(page_title="Leanne's Gloom Dashboard",
